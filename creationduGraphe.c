2880f023f7eb82e835d7e194b544cf4c0f9b18c9
Fernando Marreros <fernandomk2017@gmail.com>
2021-05-12 14:28:02 +0200

    Validation file et colonne vide

--- creationduGraphe.c
+++ creationduGraphe.c
@@ -159,6 +159,41 @@ void Print_Moitie_Matrix(array_Ty *matrix, int size)
     }
 }
 
+int validationceroxfile(array_Ty *matrix2, int sommet, int nbr_vertices)
+{
+    int valnew = 0;
+    l2 = matrix2[sommet].list;
+    for (int i = 0; i < nbr_vertices; i++)
+    {
+        if (l2->valeur == 1)
+        {
+            valnew = 1;
+        }
+        l2 = l2->next;
+    }
+    return valnew;
+}
+
+int validationceroxcolonne(array_Ty *matrix2, int file, int nbr_vertices)
+{
+    int valnew = 0;
+    
+    for (int i = 0; i < nbr_vertices; i++)
+    {
+        l2 = matrix2[i].list;
+
+        for (int j = 0; i < file; i++)
+        {    
+          l2=l2->next; 
+        }
+        if(l2->valeur == 1)
+        {   
+            valnew = 1;
+        }
+    }
+    return valnew;
+}
+
 void FairCopiedeMatrix(array_Ty *matrix1, array_Ty *matrix2, int nbr_vertices)
 {
     printf("\n Matrice du graphe \n");
@@ -192,11 +227,21 @@ void FairCopiedeMatrix(array_Ty *matrix1, array_Ty *matrix2, int nbr_vertices)
 
 void main()
 {
-    nbr_Vertices = rand();
+    nbr_Vertices = 6;
     printf("nb_sommets:%d\n", nbr_Vertices);
     array_Ty *Matrix1 = CreateMatrix();
     array_Ty *Matrix2 = CreateMatrix_vide();
     FairCopiedeMatrix(Matrix1, Matrix2, nbr_Vertices);
     PrintMatrix(Matrix2, nbr_Vertices);
-   
+    printf("\nC'es connexe la sommet :%d   -->%d", 0, validationceroxfile(Matrix2, 0, nbr_Vertices));
+    printf("\nC'es connexe la sommet :%d   -->%d", 1, validationceroxfile(Matrix2, 1, nbr_Vertices));
+    printf("\nC'es connexe la sommet :%d   -->%d", 2, validationceroxfile(Matrix2, 2, nbr_Vertices));
+    printf("\nC'es connexe la sommet :%d   -->%d", 3, validationceroxfile(Matrix2, 3, nbr_Vertices));
+
+    printf("\n\n\nFile :%d   -->%d", 0, validationceroxcolonne(Matrix2,0,nbr_Vertices));
+    printf("\n\n\nFile :%d   -->%d", 1, validationceroxcolonne(Matrix2,1,nbr_Vertices));
+    printf("\n\n\nFile :%d   -->%d", 2, validationceroxcolonne(Matrix2,2,nbr_Vertices));
+    printf("\n\n\nFile :%d   -->%d", 3, validationceroxcolonne(Matrix2,3,nbr_Vertices));
+    
+
 }
