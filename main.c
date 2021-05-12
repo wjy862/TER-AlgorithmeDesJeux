7fa6eb90f6bd6930d548f5aec1d6a8195b5c6776
wjy862 <leowjy123456@gmail.com>
2021-05-13 01:25:41 +0530

    refactoring

--- main.c
+++ main.c
@@ -16,25 +16,32 @@ int cliqueMaximale=3;
 
 int main() {
     /*Génerer un graphe*/
-    /*
     nbr_Vertices = 4;
     printf("nb_sommets:%d\n", nbr_Vertices);
     array_Ty *Matrix1 = CreateMatrix();
     array_Ty *Matrix2 = CreateMatrix_vide();
-    */
+    FairCopiedeMatrix(Matrix1, Matrix2, nbr_Vertices);
+    PrintMatrix(Matrix2, nbr_Vertices);
+
     /*Trouver un nombre de coloration propre par heursitique*/
     couleur=heuristiqueColoration(tailleArret,matrice);
+
     /*Initialisation de l'arbitre*/
     Arbitre pArbitre=setArbitre(tailleSommet,tailleArret,matrice);
+
     /*Colorier le graphe selon la vecteur stochastique des sommets*/
     Clorier(pArbitre);
+
     /*calculer des conflit de chaque sommet*/
     calculerConflit(pArbitre);
+
     /*calculer des bénéfice de chaque sommet*/
     calculerBenefice(pArbitre);
+
+    /*Imprimer tout la stucture Arbitre*/
     printArbitre(pArbitre);
 
-
+    /*free les mémoires*/
     for (int i = 0; i < tailleArret; ++i) {
         free(pArbitre->listeSommet[i]->vecteurStochastique);
         free(pArbitre->listeSommet[i]);
