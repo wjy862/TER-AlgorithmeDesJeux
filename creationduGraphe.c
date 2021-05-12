3309afed05cdc789c4ce5fee863082c4f4cface5
Fernando Marreros <fernandomk2017@gmail.com>
2021-05-12 23:32:32 +0200

    fix memoire et creation de la fonction Prendre_SommetnonConnexes

--- creationduGraphe.c
+++ creationduGraphe.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 
 typedef struct array_Ty array_Ty;
 struct array_Ty
@@ -53,7 +53,6 @@ struct array_Ty *CreateMatrix_vide()
 
 struct array_Ty *CreateMatrix()
 {
-
     struct array_Ty *Matrix;
 
     Matrix = malloc(nbr_Vertices * sizeof(*Matrix));
@@ -146,22 +145,6 @@ int RechercherValeurdeMatrix(array_Ty *matrix, int sommet, int position_file)
     return val;
 }
 
-void Print_Moitie_Matrix(array_Ty *matrix, int size)
-{
-    l = malloc(sizeof(struct list_Ty));
-    for (int i = 0; i < size; i++)
-    {
-        l = matrix[i].list;
-        for (int j = i; j < size - 1; j++)
-        {
-            printf("\nmmmmm\n");
-            printf("\ni:|%d->j:%d == %d\n", i, l->sommet, RechercherValeurdeMatrix(matrix, j, i));
-            l->next = malloc(sizeof(struct list_Ty));
-            l = l->next;
-        }
-    }
-}
-
 int validationceroxfile(array_Ty *matrix2, int sommet, int nbr_vertices)
 {
     l2 = malloc(sizeof(struct list_Ty));
@@ -189,7 +172,7 @@ int validationceroxcolonne(array_Ty *matrix2, int file, int nbr_vertices)
 
         for (int j = 0; i < file; i++)
         {
-            //l2->next = malloc(sizeof(struct list_Ty));
+            l2->next = malloc(sizeof(struct list_Ty));
             l2 = l2->next;
         }
         if (l2->valeur == 1)
@@ -202,9 +185,9 @@ int validationceroxcolonne(array_Ty *matrix2, int file, int nbr_vertices)
 
 void FairCopiedeMatrix(array_Ty *matrix1, array_Ty *matrix2, int nbr_vertices)
 {
-    //l1 = malloc(sizeof(struct list_Ty));
-    //l2 = malloc(sizeof(struct list_Ty));
-    printf("\n Matrice du graphe \n");
+    l1 = malloc(sizeof(struct list_Ty));
+    l2 = malloc(sizeof(struct list_Ty));
+    printf("\n Matrice construi du graphe \n");
     for (int i = 0; i < nbr_vertices; i++)
     {
         l1 = matrix1[i].list;
@@ -248,21 +231,23 @@ struct list_Ty *Prendre_sommetsnonconnexes(array_Ty *matrix2, int nbr_vertices)
         {
             sommets_nonconnexes->sommet = i;
             sommets_nonconnexes->valeur = 0;
-            printf("sommetnulle: %d\t", sommets_nonconnexes->sommet);
-            printf("file: %d , colonne: %d \n", validationceroxfile(matrix2, i, nbr_Vertices), validationceroxcolonne(matrix2, i, nbr_Vertices));
+            
+            printf("Nulles: file: %d , colonne: %d \n", validationceroxfile(matrix2, sommets_nonconnexes->sommet, nbr_Vertices), validationceroxcolonne(matrix2, sommets_nonconnexes->sommet, nbr_Vertices));
+            sommets_nonconnexes->next = malloc(sizeof(struct list_Ty));
+            sommets_nonconnexes = sommets_nonconnexes->next;
         }
-        sommets_nonconnexes = sommets_nonconnexes->next;
     }
     return sommets_nonconnexes;
 }
 
 void main()
 {
-    nbr_Vertices = 3;
+    nbr_Vertices = 20;
     printf("nb_sommets:%d\n", nbr_Vertices);
     array_Ty *Matrix1 = CreateMatrix();
     array_Ty *Matrix2 = CreateMatrix_vide();
     FairCopiedeMatrix(Matrix1, Matrix2, nbr_Vertices);
-    PrintMatrix(Matrix2, nbr_Vertices);
+    //PrintMatrix(Matrix2, nbr_Vertices);
     list_Ty *lsosmmets = Prendre_sommetsnonconnexes(Matrix2, nbr_Vertices);
+    printf("\n--execution fini--");
 }
