ce87862b9d848505a3ad2292f698db231ce3a8c8
uvsq21700262 <franck.naze78@gmail.com>
2021-05-13 01:17:53 +0200

    ajout des fonctions dans header et fonction sous graphe

--- src/sommet.c
+++ src/sommet.c
@@ -102,35 +102,108 @@ Arbitre genererUnGrapheConnexe(int** matrice,int m,int* visiter,int* final,int* 
     {
         for(int i =0;i<*cpt;i++)
         {
-            ret.matrice[i] = malloc(m*sizeof(int));
-            for(int j =0;j<m;j++)
+            ret.matrice[i] = malloc(*cpt*sizeof(int));
+            for(int j =0;j<*cpt;j++)
             {
                 ret.matrice[i][j]=0;
             }
         }
     }
-    int compteur = 0;
+    int compteur_i = 0;
+    int compteur_j = 0;
     for(int i =0;i<m;i++)
     {
         if(tmp[i])
         {
             for(int j=0;j<m;j++)
             {
-                ret.matrice[compteur][j] = matrice[i][j];
+                if(tmp[j])
+                {
+                    ret.matrice[compteur_i][compteur_j] = matrice[i][j];
+                    compteur_j++;
+                }
             }
-            compteur++;
+            compteur_i++;
+            compteur_j = 0;
         }
     }
-    //printf("\n");
+
     free(tmp);
     return ret;
 }
 
+int** getSousGraphe(Arbitre arb,int index,int size,int* taille)
+{
+    if(index >= size)
+    {
+        printf("l'index ne correspond à aucun sommet\n");
+            return NULL;
+    } 
+
+    int cpt = 1;
+    int cpt_i = 0;
+    int cpt_j = 0;
+    int* tmp = malloc(size*sizeof(int));
+    int **ret;
+    for(int i=0;i<size;i++)
+    {
+        tmp[i] =0;
+    }    
+    for(int i=0;i<size;i++)
+    {
+        //printf("%d ",arb.matrice[index][i]);
+        if(arb.matrice[index][i] == 1)
+        {
+            tmp[i]=1;//on ajoute les voisins du sommet index
+            cpt++;
+        }
+    }
+    //printf("cpt sous graphe = %d\n",cpt);
+    ret = malloc(cpt*sizeof(int*));
+    for(int i =0;i<cpt;i++)
+    {
+        ret[i] = malloc(cpt*sizeof(int));
+        for(int j =0;j<cpt;j++)
+        {
+            ret[i][j] = 0;
+        }
+    }
+    for(int i = 1;i<cpt;i++)
+    {
+        ret[0][i]= 1;
+        ret[i][0] =1;
+    }
+    *taille = cpt;
+    cpt_i = 1;
+    cpt_j = 0;
+    for(int i =0;i<size;i++)//on cherche les voisins du sommet index
+    {
+        if(tmp[i] == 0) continue;//pas un sommet du sous graphe
+        //printf("%d\t",i+1);
+        for(int j=0;j<size;j++)
+        {
+            if(tmp[j])//pas un sommet du sous graphe|| se regarde lui même
+            {
+                cpt_j ++;//tmp vaut 1 donc on incrémente cpt_j
+                if(arb.matrice[i][j] ==1)
+                {
+                    printf("i:%d j:%d\n",i+1,j+1);
+                    ret[cpt_i][cpt_j] = 1;
+                }
+            }
+        }
+        cpt_i++;
+        cpt_j = 0;
+    }
+    free(tmp);
+    return ret;
+}
 
 int main()
 {
-    int sommet = 25;
-    int cpt = 0;
+    int sommet = 10;
+    int degre = 3;
+    int size = 0;
     int ** adjacence = malloc(sommet* sizeof(int*));
     for(int i = 0;i<sommet;i++)
     {
@@ -140,7 +213,7 @@ int main()
             adjacence[i][j] =0;
         }
     }
-    genererUnGraphe(adjacence,sommet,1);
+    genererUnGraphe(adjacence,sommet,degre);
     int* visiter = malloc(sommet*sizeof(int));
 
     int* final = malloc(sommet*sizeof(int));
@@ -150,24 +223,28 @@ int main()
         visiter[i] = 0;
         final[i] = 0;
     }
-    Arbitre res = genererUnGrapheConnexe(adjacence,sommet,visiter,final,&cpt);
-    for(int i=0;i<13;i++)
+    Arbitre res = genererUnGrapheConnexe(adjacence,sommet,visiter,final,&size);
+    int** sousgraphe = NULL;
+    int taille = 0;
+    sousgraphe = getSousGraphe(res,9,size,&taille);  
+    for(int i=0;i<size;i++)
     {
-        for(int j=0;j<sommet;j++)
+        for(int j=0;j<size;j++)
         {
             printf("%d ",res.matrice[i][j]);
         }
         printf("\n");
     }
-    /*for(int i=0;i<sommet;i++)
+    printf("sous graphe:\n");
+    for(int i=0;i<taille;i++)
     {
-        for(int j=0;j<sommet;j++)
+        for(int j=0;j<taille;j++)
         {
-            printf("%d ",test.matrice[i][j]);
+            printf("%d ",sousgraphe[i][j]);
         }
         printf("\n");
     }
-    printf("\n");*/
+    printf("\n");
 
 
 
@@ -178,7 +255,12 @@ int main()
         free(adjacence[i]);
     }
 
-    for(int i = 0;i<cpt;i++)
+    for(int i=0;i<taille;i++)
+    {
+        free(sousgraphe[i]);
+    }
+        free(sousgraphe);
+    for(int i = 0;i<size;i++)
     {
         free(res.matrice[i]);
     }
