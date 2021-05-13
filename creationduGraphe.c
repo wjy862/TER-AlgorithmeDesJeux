5b3a6713a9e875729fdc59a1080357f4d43bba1d
Fernando Marreros <fernandomk2017@gmail.com>
2021-05-13 02:22:17 +0200

    fix de memoire et creation de la fonction qui montre le nombre des sommets a effacer parce que ils ne sont pas connexes

--- creationduGraphe.c
+++ creationduGraphe.c
@@ -16,8 +16,8 @@ struct list_Ty
     struct list_Ty *next;
 };
 
-unsigned int nbr_Vertices, taille_a_reduire_desommetsconnexes;
-struct list_Ty *list, *l, *l1, *l2, *lsommets_nonconnexes;
+unsigned int nbr_Vertices, taille_a_reduire_desommets_nonconnexes;
+struct list_Ty *list, *l, *l1, *l2;
 
 struct array_Ty *CreateMatrix_vide()
 {
@@ -67,7 +67,7 @@ struct array_Ty *CreateMatrix()
         {
             if (j > i)
             {
-                int n = 0; //rand() % 2;
+                int n = rand() % 2;
                 if (!Matrix[i].list)
                     Matrix[i].list = list;
                 else
@@ -119,9 +119,6 @@ void PrintMatrix(array_Ty *matrix, int size)
         while (a < size)
         {
             printf("%d\t", l->valeur);
-
-            l->next = malloc(sizeof(struct list_Ty));
-
             l = l->next;
             a++;
         }
@@ -225,64 +222,56 @@ struct array_Ty *Prendre_sommetsnonconnexes(array_Ty *matrix2, int nbr_vertices)
 {
     struct array_Ty *Matrix;
 
-    Matrix = malloc(1 * sizeof(*Matrix));
+    Matrix = malloc(nbr_Vertices * sizeof(*Matrix));
 
     Matrix[0].sommet = 0;
     Matrix[0].list = NULL;
     list = malloc(sizeof(struct list_Ty));
+    Matrix[0].list = list;
+    list->sommet = 0;
+    list->valeur = 0;
+    list->next = NULL;
+
+    unsigned int c = 0;
 
     for (int i = 0; i < nbr_Vertices; i++)
     {
-        if (!Matrix[0].list)
-            Matrix[0].list = list;
-        else
-        {
-            list->next = malloc(sizeof(struct list_Ty));
-            list = list->next;
-        }
-
         if (validationceroxfile(matrix2, i, nbr_Vertices) == 0 && validationceroxcolonne(matrix2, i, nbr_Vertices) == 0)
         {
-            list->sommet = i;
-            list->valeur = 1;
-            printf("Nulles: file: %d , colonne: %d \n", validationceroxfile(matrix2, list->sommet, nbr_Vertices), validationceroxcolonne(matrix2, list->sommet, nbr_Vertices));
-            list->next = NULL;
+            list->sommet = c;
+            list->valeur = i;
+            //printf("Nulles: file: %d , colonne: %d \n", validationceroxfile(matrix2, i, nbr_Vertices), validationceroxcolonne(matrix2, i, nbr_Vertices));
+            list->next = malloc(sizeof(struct list_Ty));
+            list = list->next;
+            c++;
         }
     }
 
     return Matrix;
 }
 
- unsigned int Nombre_Iterations_sommets_non_connexes(array_Ty *Matrix_sommets_non_connexes)
-{ 
+unsigned int Nombre_effacer_sommets(array_Ty *Matrix_sommets_non_connexes, int nbr_vertices)
+{
     l = malloc(sizeof(struct list_Ty));
-    l = matrix[0].list;
+    l = Matrix_sommets_non_connexes[0].list;
     int counter = 0;
-        int a = 0;
-        while (a < size)
-        {
-            printf("%d\t", l->valeur);
-
-            list->next = malloc(sizeof(struct list_Ty));
-
-            l = l->next;
-            a++;
-        }
-        printf("\n");
+    while (l->next != NULL && counter < nbr_Vertices)
+    {
+        l = l->next;
         counter++;
-    
-}  
+    }
+    return counter;
+}
 
 void main()
 {
-    nbr_Vertices = 4;
+    nbr_Vertices = rand();
     printf("nb_sommets:%d\n", nbr_Vertices);
     array_Ty *Matrix1 = CreateMatrix();
     array_Ty *Matrix2 = CreateMatrix_vide();
     FairCopiedeMatrix(Matrix1, Matrix2, nbr_Vertices);
-    PrintMatrix(Matrix2, nbr_Vertices);
+    //PrintMatrix(Matrix2, nbr_Vertices);
     array_Ty *Matrixsommetsnonconnexes = Prendre_sommetsnonconnexes(Matrix2, nbr_Vertices);
-    //taille_a_reduire_desommetsconnexes =
-    //printf("nombre: %d", Nombre_Iterations_sommets_non_connexes(Matrixsommetsnonconnexes));
+    taille_a_reduire_desommets_nonconnexes = Nombre_effacer_sommets(Matrixsommetsnonconnexes, nbr_Vertices);
     printf("\n--execution fini--");
 }
